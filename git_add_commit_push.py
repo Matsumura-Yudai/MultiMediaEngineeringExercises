8c7a66b500c32f749dc77a35cfe3f2861f6f67c5
Matsumura-Yudai <you2002724@UD0724.>
2025-04-27 23:21:38 +0900

    Further Experiments ZINC and QM9 is Completed.

--- git_add_commit_push.py
+++ git_add_commit_push.py
@@ -3,6 +3,7 @@ import os
 # Commit comment
 print("===== Input The Comment About Pushing =====")
 comment = input()
+print("===========================================")
 
 # Push
 os.system('git commit -a -m "{}"'.format(comment))
